0ebb164eba4a56fec4d94681b50ab7ee05e1aedc
Arvinder Dhanoa <awsomearvinder@gmail.com>
2020-01-31 20:46:05 -0600

    restructure code - hopefully will actually work soon

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,75 @@
+#![feature(drain_filter)]
 use gio;
 use gio::prelude::*;
 use gtk::prelude::*;
 use gtk::EntryExt;
+use gtk::GridExt;
 use gtk::LabelExt;
 
 mod lib;
 
 #[derive(Debug, Clone)]
 struct AppSearchResults {
-    text_boxes: [gtk::Label; 5],
+    binary_results: [lib::Binary; 5],
 }
 
 impl AppSearchResults {
     pub fn new(search_query: &str) -> Self {
-        let mut text_boxes: [gtk::Label; 5] = [
+        AppSearchResults {
+            binary_results: Self::search_binaries(search_query),
+        }
+    }
+    pub fn widgets(&self) -> [gtk::Label; 5] {
+        let mut labels: [gtk::Label; 5] = [
             gtk::Label::new(None),
             gtk::Label::new(None),
             gtk::Label::new(None),
             gtk::Label::new(None),
             gtk::Label::new(None),
         ];
-        let bins = lib::Binary::get_binaries_dedup();
-        let labels = bins
-            .iter()
-            .map(|bin| gtk::Label::new(Some(&bin.name().clone().into_string().unwrap())));
-        for (i, label) in labels.enumerate() {
-            if i < 5 {
-                text_boxes[i] = label;
-            }
-        }
-        AppSearchResults {
-            text_boxes: text_boxes,
-        }
-    }
-    pub fn widgets(&self) -> &[gtk::Label; 5] {
-        &self.text_boxes
-    }
-    pub fn search(&mut self, query: &str) -> &[gtk::Label] {
-        let mut bins = lib::Binary::get_binaries_dedup();
-        for item in self.text_boxes.iter_mut() {
-            for bin in bins.iter_mut() {
-                let binName = bin.name().clone().into_string().unwrap_or_else(|e| {
-                    eprintln!("Error:{:?}", e);
+        for (i, item) in self.binary_results.iter().enumerate() {
+            labels[i] = gtk::Label::new(Some(&item.name().clone().into_string().unwrap_or_else(
+                |e| {
+                    eprintln!("Error:  {:?}", e);
                     std::process::exit(1);
-                });
-                if binName.contains(query) {
-                    eprintln!("{}", binName);
-                    item.set_text(&binName);
-                }
+                },
+            )));
+        }
+        labels
+    }
+    fn search_binaries(query: &str) -> [lib::Binary; 5] {
+        let mut binary_results: [lib::Binary; 5] = [
+            lib::Binary::new(std::ffi::OsString::new(), std::path::PathBuf::new()),
+            lib::Binary::new(std::ffi::OsString::new(), std::path::PathBuf::new()),
+            lib::Binary::new(std::ffi::OsString::new(), std::path::PathBuf::new()),
+            lib::Binary::new(std::ffi::OsString::new(), std::path::PathBuf::new()),
+            lib::Binary::new(std::ffi::OsString::new(), std::path::PathBuf::new()),
+        ];
+        let mut bins = lib::Binary::get_binaries_dedup();
+        bins.drain_filter(|item| {
+            !item
+                .name()
+                .clone()
+                .into_string()
+                .unwrap_or_else(|e| {
+                    eprintln!("Error: {:?}", e);
+                    std::process::exit(1);
+                })
+                .contains(query)
+        });
+        for (i, label) in bins.iter().enumerate() {
+            if i < 5 {
+                binary_results[i] = label.clone();
             }
         }
-        &self.text_boxes
+        binary_results
+    }
+    pub fn binaries(&self) -> &[lib::Binary; 5] {
+        &self.binary_results
+    }
+    pub fn search(&mut self, query: &str) -> [gtk::Label; 5] {
+        self.binary_results = Self::search_binaries(query);
+        self.widgets()
     }
 }
 
@@ -64,19 +83,26 @@ fn build_ui(app: &gtk::Application) {
         .default_width(600)
         .resizable(false)
         .build();
-    let entry = gtk::Entry::new();
+    let gtk_grid = std::rc::Rc::new(std::cell::RefCell::new(gtk::Grid::new()));
     let search = std::rc::Rc::new(std::cell::RefCell::new(AppSearchResults::new("")));
+    let entry = gtk::Entry::new();
+    gtk_grid.borrow_mut().add(&entry);
     let search_clone = search.clone();
+    let gtk_grid_clone = gtk_grid.clone();
     entry.connect_changed(move |entry| {
         let query = match entry.get_text() {
-            Some(T) => T.as_str().to_owned(),
+            Some(input) => input.as_str().to_owned(),
             None => "".to_owned(),
         };
         eprintln!("{:?}", &query);
         let items = search_clone.borrow_mut().search(&query).to_owned();
+        items
+            .iter()
+            .for_each(|item| gtk_grid_clone.borrow_mut().add(item));
         items.iter().for_each(|x| eprintln!("{:?}", x.get_text()))
     });
-    window.add(&entry);
+    let grid = &*(gtk_grid.borrow());
+    window.add(grid);
     window.show_all();
 }
 fn main() {
@@ -86,5 +112,4 @@ fn main() {
     });
     app.connect_activate(|app| build_ui(app));
     app.run(&std::env::args().collect::<Vec<_>>());
-    let bins = lib::Binary::get_binaries_dedup();
 }
